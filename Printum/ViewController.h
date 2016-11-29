90fbbb0549e4029a3030b3bcad2f0b412785dad1
JoyDa <joyda@joydas-imac.local>
2016-11-29 06:24:59 -0500

    Initial Commit

---
+++ Printum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Printum
+//
+//  Created by JoyDa on 11/29/16.
+//  Copyright © 2016 JoyDa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Printum/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Printum
+//
+//  Created by JoyDa on 11/29/16.
+//  Copyright © 2016 JoyDa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
